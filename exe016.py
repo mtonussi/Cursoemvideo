d95c1a4e9e47e4ccbd24d1409061f9104d5b19db
mtonussi <murilotonussi@hotmail.com>
2022-04-26 09:35:14 -0300

    ok

--- exe016.py
+++ exe016.py
@@ -1,3 +1,11 @@
-import math
-num = int(input('Digite um número: '))
+'''from math import trunc
+num = float(input('Digite um número: '))
+print('A parte inteira do número {} é: {}'.format(num, trunc(num)))'''
+
+num = float(input('Digite um valor: '))
+print('A parte inteira do número {} é: {}'.format(num, int(num)))
+
+
+
+
 
---
+++ extra-aula08.py
@@ -0,0 +1,10 @@
+from math import sqrt, floor
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num,floor(raiz)))
+
+
+
+
+
+
