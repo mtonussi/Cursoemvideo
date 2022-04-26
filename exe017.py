267516265e4df1975518de2c6028ad9bec461e94
mtonussi <murilotonussi@hotmail.com>
2022-04-26 09:37:19 -0300

    n.content

--- exe017.py
+++ exe017.py
@@ -1,10 +1,10 @@
-'''co = float(input('Qual o comprimento do catato oposto? '))
+'''co = float(input('Qual o comprimento do cateto oposto? '))
 ca = float(input('Qual o comprimento do cateto adjacente? '))
 hip = (co ** 2 + ca ** 2) ** (1/2)
 print('A hipotenusa deve medir {}'.format(hip))'''
 
 import math
-co = float(input('Qual o comprimento do catato oposto? '))
+co = float(input('Qual o comprimento do cateto oposto? '))
 ca = float(input('Qual o comprimento do cateto adjacente? '))
 hip = math.hypot(co,ca)
 print('A hipotenusa deve medir {:.2f}'.format(hip))
